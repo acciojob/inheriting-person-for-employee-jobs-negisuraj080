ec3a9d88ecf2bf06d25b452bc5efaa5f37783110
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2024-02-26 16:03:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // complete this js code
-function Person(name, age) {
-	constructor(name,age){
+class Person {
+	constructor (name, age) {
 		this.name = name;
 		this.age = age;
 	}
 	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`)
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`)
 	}
 }
 
